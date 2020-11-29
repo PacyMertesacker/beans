5dca85891d3f0e4ed893370fb5fce58ede459578
ronankelly <ronankelly11@gmail.com>
2020-11-29 21:00:41 +0000

    Added some Cassandra Input and Delete methods. Added test data. Added a Note.txt as a high-level guide for how Cassandra will function

--- cassandra/src/main/java/Main.java
+++ cassandra/src/main/java/Main.java
@@ -5,18 +5,21 @@ import com.datastax.driver.core.Row;
 import com.datastax.driver.core.exceptions.AlreadyExistsException;
 
 public class Main {
+    // Static session so it is accessible by the CRUD methods:
+    private static Session session;
+
     public static void main(String[] args) {
         try {
             String serverIp = "127.0.0.1";
             String keyspace = "system";
+            String exampkeyspace = "exampkeyspace"; //Keyspace is a data container, similar to a database in a RDBMS. A keyspace is an outermost object that determines how data replicates on nodes.
 
             // Create Cassandra cluster:
             Cluster cluster = Cluster.builder()
                 .addContactPoints(serverIp)
                 .build();
 
-            Session session = cluster.connect();
-            String exampkeyspace = "exampkeyspace";
+            session = cluster.connect();
             String statement = "CREATE KEYSPACE "+ exampkeyspace +" WITH replication = {'class':'SimpleStrategy','replication_factor':1}";
 
             try {
@@ -46,20 +49,18 @@ public class Main {
                 }
             }
 
-            String cqlStatementC = "INSERT INTO exampkeyspace.users (username, password) " + 
-                      "VALUES ('Serenity', 'fa3dfQefx')";
-
             String cqlStatementU = "UPDATE exampkeyspace.users " +
                       "SET password = 'zzaEcvAf32hla'," +
                       "WHERE username = 'Serenity';";
 
-            String cqlStatementD = "DELETE FROM exampkeyspace.users " + 
-                      "WHERE username = 'Serenity';";
-
-            session.execute(cqlStatementC);
-
             String cqlStatement = "SELECT * FROM exampkeyspace.users";
+            System.out.println(session.execute(cqlStatement).one());
             
+            for (String row: users){
+                String[] user = row.split(",");
+                insertUser(user[0],user[1]);
+            }
+
             for (Row row : session.execute(cqlStatement)) {
                 System.out.println(row.toString());
             }
@@ -76,14 +77,25 @@ public class Main {
         }
     }
 
+    public static void insertUser(String user, String password){
+        session.execute("INSERT INTO exampkeyspace.users (username, password) " + 
+        " VALUES ('" + user + "', '" + password + "')");
+    }
+
+    public static void deleteUser(String user){
+        session.execute("DELETE FROM exampkeyspace.users " + 
+                      "WHERE username = '" + user + "';");
+    }
+
+
     /**
     Test Data
      */
     private static final String [] users = {
-        new String("Adam, Password1"),
-        new String("Beth, Password2"),
-        new String("Carl, Password3"),
-        new String("Daniel, Password4"),
-        new String("Edith, Password5")
+        new String("Adam,Password1"),
+        new String("Beth,Password2"),
+        new String("Carl,Password3"),
+        new String("Daniel,Password4"),
+        new String("Edith,Password5")
     };
 }
