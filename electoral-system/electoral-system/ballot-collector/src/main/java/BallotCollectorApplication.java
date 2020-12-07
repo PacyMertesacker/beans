f3eb23fedca49b0cd5b5b416ce4964699c1be4c8
AdamWaldron98 <44497139+AdamWaldron98@users.noreply.github.com>
2020-12-07 16:17:24 +0000

    Moved ballot-collector main

--- electoral-system/electoral-system/ballot-collector/src/main/java/BallotCollectorApplication.java
+++ electoral-system/electoral-system/ballot-collector/src/main/java/BallotCollectorApplication.java
@@ -1,4 +1,3 @@
-package ballotCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- electoral-system/electoral-system/candidate/src/main/java/candidate/CandidateApplication.java
+++ electoral-system/electoral-system/candidate/src/main/java/candidate/CandidateApplication.java
@@ -1,11 +1,9 @@
 package candidate;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CandidateApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CandidateApplication.class, args);
     }
