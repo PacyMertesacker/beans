e19bb58f523c2e0a358c7f29bacafdba262cfb6c
seanennis <seanennis98@gmail.com>
2020-12-08 17:01:18 +0000

    Fixed errors for candidate connecting to cassandra

--- electoral-system/electoral-system/core/src/main/java/core/exception/ApiExceptionHandler.java
+++ electoral-system/electoral-system/core/src/main/java/core/exception/ApiExceptionHandler.java
@@ -8,13 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
-import core.exception.ApiRequestException;
-
-/**
- * Exception handle for customized exceptions to be used in Community Connect API.
- * @author Sean Ennis O'Toole
- */
-
 @ControllerAdvice
 public class ApiExceptionHandler {
 
