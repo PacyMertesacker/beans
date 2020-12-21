0cde082d150cbbc97ce2422738b8e0aaa14ca9bd
PacyMertesacker <a.s.oreilly99@gmail.com>
2020-12-21 17:36:12 +0000

    created shell for eureka server + added annotations

--- electoral-system/electoral-system/candidate/src/main/java/candidate/CandidateApplication.java
+++ electoral-system/electoral-system/candidate/src/main/java/candidate/CandidateApplication.java
@@ -1,8 +1,10 @@
 package candidate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CandidateApplication {
     public static void main(String[] args) {
         SpringApplication.run(CandidateApplication.class, args);
--- electoral-system/electoral-system/system-registration-manager/src/main/java/systemregistrationmanager/SystemRegistrationManagerApplication.java
+++ electoral-system/electoral-system/system-registration-manager/src/main/java/systemregistrationmanager/SystemRegistrationManagerApplication.java
@@ -2,8 +2,10 @@ package systemregistrationmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemRegistrationManagerApplication {
 
 	public static void main(String[] args) {
