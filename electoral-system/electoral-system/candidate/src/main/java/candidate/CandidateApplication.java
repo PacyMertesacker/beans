faeb5fdd2a025f08d2b1a9bf94b18a43d511ae8a
AdamWaldron98 <44497139+AdamWaldron98@users.noreply.github.com>
2021-01-04 18:35:19 +0000

    Revert "Merge branch 'master' of https://gitlab.com/ronankelly11/beans"
    This reverts commit d70b6f06c3184bb5ed9aa7b3971bcba14a03ace5, reversing
    changes made to b122c67b612d2d0dcd0c27a76741b0ed47f51662.

---
+++ electoral-system/electoral-system/candidate/src/main/java/candidate/CandidateApplication.java
@@ -0,0 +1,13 @@
+package candidate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CandidateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CandidateApplication.class, args);
+    }
+
+}
