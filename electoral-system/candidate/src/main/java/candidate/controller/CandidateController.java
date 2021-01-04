fa07970e98ed6c2ca37f752cda304d004b24ce6d
AdamWaldron98 <44497139+AdamWaldron98@users.noreply.github.com>
2021-01-04 23:46:22 +0000

    Vin d
    Reduced canddiates to two
    Modified voter info to match canddiates and regions

--- electoral-system/candidate/src/main/java/candidate/controller/CandidateController.java
+++ electoral-system/candidate/src/main/java/candidate/controller/CandidateController.java
@@ -2,21 +2,16 @@ package candidate.controller;
 
 import core.entity.Candidate;
 import core.exception.ApiRequestException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     public static final Candidate[] candidates = {
-            new Candidate("Sean Ennis O'Toole", "PartyA", "Blah Blah Blah PartyA Blah Blah Blah"),
-            new Candidate("Adam Shorten", "The Adam Party", "Will strengthen the Adam population"),
-            new Candidate("Luke Murphy", "PartyB", "Blah Blah Blah PartyB Blah Blah Blah"),
-            new Candidate("Adam Waldron", "The Adam Party", "Will encourage cat owners to consider the name Adam for their pets"),
-            new Candidate("Ronan Kelly", "PartyC", "Blah Blah Blah PartyC Blah Blah Blah")
+            new Candidate("Vin Diesel with hair", "TheFamily", "Blah Blah Blah TheFamily Blah Blah Blah"),
+            new Candidate("Vin Diesel", "FastAndFurious", "waddyamean"),
     };
 
     @GetMapping("/name/{name}")
