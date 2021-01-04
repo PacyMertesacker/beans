39cdc01287c17cde6dde9438d7f8c19865c137f6
seanennis <seanennis98@gmail.com>
2021-01-04 23:57:09 +0000

    Ballot collectors now only check their designated region

--- electoral-system/ballot-collector/src/main/java/ballotCollector/BallotCollectorApplication.java
+++ electoral-system/ballot-collector/src/main/java/ballotCollector/BallotCollectorApplication.java
@@ -11,5 +11,4 @@ public class BallotCollectorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BallotCollectorApplication.class, args);
 	}
-
 }
--- electoral-system/ballot-collector/src/main/java/ballotCollector/controller/BallotCollectorController.java
+++ electoral-system/ballot-collector/src/main/java/ballotCollector/controller/BallotCollectorController.java
@@ -1,6 +1,7 @@
 package ballotCollector.controller;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,34 +10,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import core.entity.Voter;
+import core.entity.Candidate;
 
 @RestController
 @RequestMapping("/ballotcollector")
 public class BallotCollectorController {
-    HashMap<String,HashMap<String,Integer>> regionMap = new HashMap<>();
+    HashMap<String,Integer> voteMap = new HashMap<>();
+    ArrayList<Candidate> candidates = new ArrayList<>();
 
     @PostMapping("/voter")
     public void addVote(@RequestBody Voter voter) {
-        HashMap<String, Integer> currRegionMap = regionMap.get(voter.getRegion());
-        if(currRegionMap == null){
-            currRegionMap = new HashMap<String,Integer>(); 
-        }
+        // only count vote if it is valid
+        if(candidates.contains(voter.getVotedFor()) {
+            // only count votes for current region
+            if(voter.getRegion().equals("A")) {
+                Integer numVotes = voteMap.get(voter.getVotedFor());
+                if (numVotes == null) {
+                    numVotes = 0;
+                }
 
-        Integer numVotes = currRegionMap.get(voter.getVotedFor()); 
-        if(numVotes == null){
-            numVotes = 0;
+                voteMap.put(voter.getVotedFor(), ++numVotes);
+                System.out.println(regionMap.toString());
+            }
         }
-
-        currRegionMap.put(voter.getVotedFor(), ++numVotes);
-        regionMap.put(voter.getRegion(), currRegionMap);
-        System.out.println(regionMap.toString());
     }
 
-    public Integer test(String candidate, String region){
+  /*  public Integer test(String candidate, String region){
         HashMap<String, Integer> currRegionMap = regionMap.get(region);
         Integer numVotes = currRegionMap.get(candidate); 
         
         return numVotes;
+    } */
+
+    @PostMapping("/candidate")
+    public void addCandidate(@RequestBody Candidate candidate) {
+        this.candidates.add(candidate);
+        System.out.println(candidate.toString());
     }
 
     @GetMapping("/test")
